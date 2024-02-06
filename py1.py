6139e7b6db99f47e5d7c2d28b165b7ad976d7cb1
AlexRobbin <158841503+AlexRobbin@users.noreply.github.com>
2024-02-06 19:09:42 +0330

    Add files via upload
    first Practice

---
+++ py1.py
@@ -0,0 +1,45 @@
+#............dictionary and concatenation...............
+'''
+x = {
+    'name': 'ali' , 'last': 'alinezhad'
+}
+result = "my name is " + x['name'] + " and my lastname is " + x['last']
+print(result)
+'''
+# ..........end.................
+
+
+#..........concatenation with f and dictionary........
+'''
+x = {'name': 'ali' , 'last': 'alinezhad'}
+result =(f" my name is {x['name']} and my last name is {x['last']}")
+print(result)
+'''
+# ..........end.................
+
+
+#..........indexing........
+'''
+x = "alireza"
+print(x[1])
+print(x[3:])
+print(x[0:2])
+'''
+# ..........end.................
+
+#...............converting................
+weight = 80.500
+weight = (int(weight)) +1 # Rounding number to up
+weight = (int(weight)) -1 # Rounding number to down
+
+print(weight) 
+print(type(int(weight))) # convert to another variable 
+
+
+# ..........end.................
+
+
+
+
+
+
